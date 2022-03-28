57b423bd60c6674558b04a14253b7d1fe8114f02
zhangrengu123_kSM <1005006793@qq.com>
2022-03-28 23:08:47 +0800

    面板组件

---
+++ src/views/Home/components/home-panel.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+interface Props {
+  title: string;
+  subTitle?: string;
+}
+defineProps<Props>();
+</script>
+
+<template>
+  <div class="home-panel">
+    <div class="container">
+      <div class="head">
+        <h3>
+          {{ title }}
+          <small>{{ subTitle }}</small>
+        </h3>
+        <!-- 右侧内容区域 -->
+        <slot name="right"></slot>
+      </div>
+      <!-- 主体内容区域 -->
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="less">
+.home-panel {
+  background-color: #fff;
+  .head {
+    padding: 40px 0;
+    display: flex;
+    align-items: flex-end;
+    h3 {
+      flex: 1;
+      font-size: 32px;
+      font-weight: normal;
+      margin-left: 6px;
+      height: 35px;
+      line-height: 35px;
+      small {
+        font-size: 16px;
+        color: #999;
+        margin-left: 20px;
+      }
+    }
+  }
+}
+</style>
