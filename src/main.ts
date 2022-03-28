6579c6e4c5516fc9f18ef25785d7fa7e7ee43364
zhangrengu123_kSM <1005006793@qq.com>
2022-03-28 22:31:00 +0800

    全局组件注册

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,12 @@ import router from "./router";
 import "normalize.css";
 // 按照项目需求，提供自己的公用样式
 import "@/assets/styles/common.less";
+import XtxUI from "./components/XtxUI";
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
+app.use(XtxUI)
 
 app.mount("#app");
